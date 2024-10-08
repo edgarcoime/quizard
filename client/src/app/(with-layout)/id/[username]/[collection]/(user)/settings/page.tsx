e6d29ab26a0bdea20075285bae55b68d7adff4a4
Edgar Coime <e.coimeii@hotmail.com>
2024-10-07 20:14:08 -0700

    feat: finished card settings page

--- client/src/app/(with-layout)/id/[username]/[collection]/(user)/settings/page.tsx
+++ client/src/app/(with-layout)/id/[username]/[collection]/(user)/settings/page.tsx
@@ -22,6 +22,7 @@ export default async function Page({
   return (
     <div className="p-5 grid gap-5">
       <h1 className="text-2xl">Collection Settings</h1>
+      <p className="font-serif text-gray-500">{`Editting Settings for "${collection}"`}</p>
       <GeneralSection collectionName={collection} />
       <StatisticsSection />
     </div>
