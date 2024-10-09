e6a5bbb4f5d9778ea7c8ffaa05b4cb18e68698c8
Reo Tamai <reotamai5@gmail.com>
2024-10-09 14:31:19 -0700

    fix bug in deployed version

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import APIRouter, HTTPException, Request, Depends
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from config.settings import settings
 from config.database import get_db
 from core.auth import create_session, delete_session, extend_session, get_sessions, oauth, verify_user
 from core.user import (
@@ -16,7 +17,6 @@ router = APIRouter()
 
 @router.get("/login/{provider}")
 async def login(provider: str, request: Request, redirect_to: Optional[str] = None):
-    redirect_uri = request.url_for("auth_callback", provider=provider)
     client = oauth.create_client(provider)
 
     referer = request.headers.get("referer")
@@ -29,7 +29,7 @@ async def login(provider: str, request: Request, redirect_to: Optional[str] = No
         else:
             request.session['redirect_to'] = referer
 
-    return await client.authorize_redirect(request, redirect_uri)  # type: ignore
+    return await client.authorize_redirect(request, settings.HOST + "/api/py/auth/callback/" + provider)  # type: ignore
 
 
 @router.get("/callback/{provider}")
