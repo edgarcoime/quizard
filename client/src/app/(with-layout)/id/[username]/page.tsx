329c7e722bb1778bded6b86e38c46c130f214556
Edgar Coime <e.coimeii@hotmail.com>
2024-10-10 10:41:05 -0700

    feat: refactored collection page

--- client/src/app/(with-layout)/id/[username]/page.tsx
+++ client/src/app/(with-layout)/id/[username]/page.tsx
@@ -1,46 +1,29 @@
 import { Button } from "@/components/ui/button";
 import SettingsButton from "@/components/ui/settingsButton";
-import Link from "next/link";
-import { headers } from 'next/headers'
-import {cookies} from 'next/headers'
+import { headers } from "next/headers";
+import CollectionView from "./CollectionView";
 
 // example of server side fetching
 // - Since this fetch originate from the nextjs server, header needs to be transported so backend can identify the user
-async function getData () {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/py/user/me`, {credentials: "include", headers: headers()})
-    const data = await res.text()
-    return data
+async function getData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/py/user/me`, {
+    credentials: "include",
+    headers: headers(),
+  });
+  const data = await res.text();
+  return data;
 }
 
-export default async function Page({ params }: { params: { username: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: { username: string };
+}) {
   const { username } = params;
-
-  const sampleCollections = [
-    { Name: "Software Engineering", collectionId: 1 },
-    { Name: "Calculus", collectionId: 2 },
-    { Name: "Operating Systems", collectionId: 3 },
-    { Name: "Network security", collectionId: 4 },
-  ];
-
-  let list_of_buttons: any = [];
-  sampleCollections.map((collection) => {
-    let id = collection.Name;
-    list_of_buttons.push(
-      <Link href={`/id/${username}/${id}`}>
-        <Button
-          key={collection.collectionId}
-          className="w-full sm:w-auto p-8 bg-slate-300"
-          variant="outline"
-        >
-          {collection.Name}
-        </Button>
-      </Link>,
-    );
-  });
 
   const settingsRoute = `/id/${username}/settings`;
 
-  const data = await getData()
+  const data = await getData();
 
   return (
     <div>
@@ -50,7 +33,7 @@ export default async function Page({ params }: { params: { username: string } })
       </div>
       <h1 className="flex flex-row justify-center m-5 text-5xl">Collections</h1>
       <div className="flex flex-col sm:flex-row justify-center gap-4 p-4">
-        {list_of_buttons}
+        <CollectionView username={username} />
       </div>
     </div>
   );
---
+++ client/src/lib/samples/data.ts
@@ -0,0 +1,6 @@
+export const collections = [
+  { Name: "Software Engineering", collectionId: 1 },
+  { Name: "Calculus", collectionId: 2 },
+  { Name: "Operating Systems", collectionId: 3 },
+  { Name: "Network security", collectionId: 4 },
+];
