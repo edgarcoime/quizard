301b2ae4f9dc5c5be74bbe8e87c59cd0d722c7f8
Reo Tamai <41591313+reotam5@users.noreply.github.com>
2024-11-01 09:53:52 -0700

    Revert "adds footer and floating button"

--- client/src/app/(with-layout)/layout.tsx
+++ client/src/app/(with-layout)/layout.tsx
@@ -1,36 +1,26 @@
 import Header from "@/components/partials/Header";
 import Footer from "@/components/partials/Footer";
-import { headers } from "next/headers";
-import { fetchUserData } from "@/lib/api/userData";
 
-export default async function PublicLayout({
+export default function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const cookie = headers().get('cookie')
-  const data = await fetchUserData(cookie ? {
-    credentials: "include",
-    headers: {
-      Cookie: cookie
-    },
-  } : {});
-  const user_name = data?.username
-  const isLoggedIn = !!data
   // Global layout here add header/nav and footers
   return (
     // Background
-    <div className="max-w-screen-xl mx-auto w-full bg-gray-100 min-h-screen flex flex-col">
+    <div className="h-screen w-screen bg-slate-500 flex flex-col">
       <Header />
 
       {/* Main content */}
       {/* max width of content should be xl or 1280px according to tailwind */}
       <main className="bg-gray-100 grow">
-        <div className="bg-gray-200 min-h-screen max-w-screen-xl mx-auto">
+        <div className="bg-gray-200 h-full max-w-screen-xl mx-auto">
           {children}
         </div>
       </main>
-      {isLoggedIn && <Footer userName={user_name} />}
+
+      <Footer />
     </div>
   );
 }
--- client/src/components/partials/Footer.tsx
+++ client/src/components/partials/Footer.tsx
@@ -1,50 +1,7 @@
-"use client"
-
-import { fetchUserData } from "@/lib/api/userData";
-import { headers } from "next/headers";
-import Link from "next/link";
-import { IoIosHome } from "react-icons/io";
-import { FaCircleUser } from "react-icons/fa6";
-import { MdAddBox } from "react-icons/md";
-import { IoAdd } from "react-icons/io5";
-import { usePathname } from "next/navigation";
-
-export default function Footer({ userName }: { userName: string }) {
-  const pathname = usePathname();
-  const isCollections = /^\/id\/(?!settings$|new$)[^\/]+\/(?!settings$|new$)[^\/]+$/.test(pathname)
-
+export default function Footer() {
   return (
-    <footer className="w-full sticky bottom-0 right-0 left-0 overflow-hidden">
-      <div className="bg-gray-500 flex flex-row h-full justify-around items-center p-2 mt-1">
-        <Link href={`/id/${userName}`} className="flex flex-col items-center hover:text-gray-300">
-          <IoIosHome style={{ fontSize: '22px' }} />
-          <span className="text-sm">
-            Home
-          </span>
-        </Link>
-        <Link href={`/id/${userName}/new`} className="flex flex-col items-center hover:text-gray-300">
-          <MdAddBox style={{ fontSize: '22px' }} />
-          <span className="text-sm">
-            New Collection
-          </span>
-        </Link>
-        <Link href={`/id/${userName}/settings`} className="flex flex-col items-center hover:text-gray-300">
-          <FaCircleUser style={{ fontSize: '22px' }} />
-          <span className="text-sm">
-            Profile
-          </span>
-        </Link>
-      </div>
-      {
-        isCollections && (
-          <Link
-            className="fixed bottom-16 right-2 rounded-full w-10 h-10 z-50 bg-white border border-black flex justify-center items-center"
-            href={`${pathname}/new`}
-          >
-            <IoAdd className="h-6 w-6" />
-          </Link>
-        )
-      }
+    <footer className="bg-gray-200 min-h-5">
+      <div className="bg-gray-500 h-full max-w-screen-xl mx-auto"></div>
     </footer>
   );
 }
