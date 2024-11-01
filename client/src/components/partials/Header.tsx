56bac57082a0b02e07b5f88a9e86644c807320bc
AyyyTran <AyyyTran@gmail.com>
2024-10-31 22:41:23 -0700

    for now just send logout to signin page

--- client/src/components/partials/Header.tsx
+++ client/src/components/partials/Header.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import NavBackButton from "./NavBackButton";
+import MobileMenu from "./MobileMenu";
 
 export default function Header() {
   return (
     <header className="bg-gray-200">
       <div className="bg-gray-500 h-full max-w-screen-xl mx-auto shadow-lg">
-        <NavBackButton />
+        {/* <NavBackButton /> */}
+        <MobileMenu/>
       </div>
     </header>
   );
