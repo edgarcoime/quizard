5c5cc3924a923a31620191c9404534bd8ed2ef10
Reo Tamai <reotamai5@gmail.com>
2024-11-03 00:04:25 -0700

    ai integration

--- server/config/settings.py
+++ server/config/settings.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     GOOGLE_CLIENT_SECRET: str = ""
     API_ROUTE_PREFIX: str = ""
     HOST: str = ""
+    GROQ_API_KEY: str = ""
+
 
     class Config:
         env_file = ".env"
