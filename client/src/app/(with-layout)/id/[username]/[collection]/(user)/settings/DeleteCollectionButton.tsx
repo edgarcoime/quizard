6974731bcb7d60be79e3a79e4203de3f8fec7f8d
Edgar Coime <e.coimeii@hotmail.com>
2024-11-03 01:55:03 -0700

    feat(col): add delete button

---
+++ client/src/app/(with-layout)/id/[username]/[collection]/(user)/settings/DeleteCollectionButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { API_BASE_URL } from "@/constants";
+import { useRouter, useParams } from "next/navigation";
+
+export default function DeleteCollectionButton() {
+  const router = useRouter();
+  const params = useParams<{ username: string; collection: string }>();
+
+  async function handleClick() {
+    console.log("Delete collection", params.collection);
+    const url = `${API_BASE_URL}/collection/${params.collection}`;
+
+    const res = await fetch(url, {
+      method: "DELETE",
+      credentials: "include",
+    });
+    console.log("Delete collection response", res);
+
+    if (!res.ok) {
+      console.error("Failed to delete collection", res.status, res.statusText);
+      return;
+    }
+
+    router.push(`/id/${params.username}`);
+  }
+
+  return (
+    <Button type="submit" variant="destructive" onClick={handleClick}>
+      Delete Collection
+    </Button>
+  );
+}
