40d218846b4ecba642f936e26911978050f5410d
AmarjotS82 <113328370+AmarjotS82@users.noreply.github.com>
2024-11-04 21:27:07 -0800

    Integrated play feature to pull from backend and give AI feedback (#38)

--- client/src/app/(with-layout)/id/[username]/[collection]/play/CardTile.tsx
+++ client/src/app/(with-layout)/id/[username]/[collection]/play/CardTile.tsx
@@ -1,5 +1,8 @@
+"use client"
+import useCardData from "@/components/hooks/useCardData";
 import { Card, CardTitle } from "@/components/ui/card"
-import { useState } from "react"
+import fetchCard from "@/lib/api/cardInfo";
+import { useEffect, useState } from "react"
 
 interface CardProps{
     question: string,
@@ -7,22 +10,47 @@ interface CardProps{
     flippedArr: boolean[],
     setFlippedArr: Function,
     index: number
+    id: string
 }
 
-function flipCard(index: number, flippedArr: boolean[], setFlipFun: Function){
+async function flipCard(index: number, isFlipped: boolean, flippedArr: boolean[], setFlipFun: Function){
     const newFlippedArr = [...flippedArr];
     newFlippedArr[index] = !newFlippedArr[index]; 
     setFlipFun(newFlippedArr);
+
+    await new Promise((resolve) => setTimeout(resolve, 400));
+
+    
 }
 
-export default function CardTile({question, answer, flippedArr, setFlippedArr, index}: CardProps){
+// async function  fetchAnswer(id: string){
+//     const data = await fetchCard(id)
+//     let answer: string = data.answers[0].answer
+//     return answer;
+
+// }
+
+export default function CardTile({question,answer,  flippedArr, setFlippedArr, index, id}: CardProps){
     let isFlipped = flippedArr[index]
-    let content = ""
-    {isFlipped ? content = answer : content = question}
+
+    // const [content, setContent] = useState(question);
+    const content = isFlipped ? answer : question;
+
+    const handleClick = async () => {
+        await flipCard(index,isFlipped, flippedArr, setFlippedArr);
+    };
+   
     return(
         <>
-            <Card className="w-full sm:w-auto p-8 bg-slate-300 flex flex-col justify-center items-center" onClick={ () => {flipCard(index, flippedArr, setFlippedArr)}}> 
-                <CardTitle className="text-center">{content}</CardTitle>
+            <Card   className={`w-full sm:w-auto p-8 bg-slate-300 flex flex-col justify-center items-center transition-transform duration-1000 `} 
+                    style={{ 
+                        perspective: "1000px", 
+                        transformStyle: "preserve-3d", 
+                        transform: isFlipped ? "rotateY(360deg)" : "rotateY(0deg)",
+                        
+                    }}
+                    onClick={handleClick}> 
+                <CardTitle className="text-center "> {content}</CardTitle>
             </Card>
         </>
     )
--- client/src/app/(with-layout)/id/[username]/[collection]/play/page.tsx
+++ client/src/app/(with-layout)/id/[username]/[collection]/play/page.tsx
@@ -1,64 +1,17 @@
-"use client"
+"use server"
+import { fetchCards } from "@/lib/api/collectionCards";
+import { PlayPageClient } from "./PlayPageClient";
 
-import { cards } from "@/lib/samples/data";
-import { useState } from "react";
-import CardTile from "./CardTile";
-import NextCardButton from "./NextCardButton";
-import PrevCardButton from "./prevCardButton";
-import AnswerForm from "./AnswerForm";
-import FeedbackCard from "./FeedbackCard";
-
-interface PlayProps{
-    params: { username: string; collection: string }
+interface PlayProps {
+  params: { username: string; collection: string };
 }
 
-
-
-export default function PlayPage({params}: PlayProps){
-    const [index, setIndex] = useState(0);
-
-    const { username, collection: collectionId } = params;
-    const decodedCollectionId = decodeURIComponent(collectionId);
-    const selectedCollection = cards.find(
-        (col) => col.collectionName.toLowerCase() === decodedCollectionId.toLowerCase(),
-    );
-    let card_question = selectedCollection?.cards[index].question
-    let card_answer = selectedCollection?.cards[index].answer
-    let total_num_of_cards = selectedCollection?.cards.length
-    const [flippedArr, setFlippedArr] = useState(Array(total_num_of_cards).fill(false))
-    const [answesArr, setAnswerArr] = useState(Array(total_num_of_cards).fill(""))
-    return(
-        <>
-            {/* Show question number */}
-            <h1 className="text-center">Question {index+1} of {total_num_of_cards}</h1> 
-
-            {/* Card tile with question */}
-            <div className="flex flex-col sm:flex-row justify-center gap-4 p-4">
-                {card_question && card_answer && <CardTile key={index} question={card_question} answer={card_answer} flippedArr={flippedArr} setFlippedArr={setFlippedArr} index={index}></CardTile> }
-            </div>
-
-            {/* Answer form where user submits answer*/}
-            <div className=" flex flex-col justify-center items-center sm:flex-row gap-4 p-4">
-            <h1>Answer:</h1>
-                <AnswerForm key={index} answerArr = {answesArr} setAnswerArr ={setAnswerArr} index={index}></AnswerForm>
-            </div>
-            
-            {/* Feedback Card for response from AI */}
-            <div className=" flex flex-col justify-center items-center sm:flex-row gap-4 p-4">
-                <FeedbackCard></FeedbackCard>
-            </div>
-
-            {/* Next and previous buttons */}
-            <div className="flex flex-row justify-between mb-5">
-                <div className="m-5">
-                    <PrevCardButton  index={index} setIndexFun={setIndex} ></PrevCardButton>
-                </div>
-
-                <div className="m-5">
-                    {total_num_of_cards && <NextCardButton index={index} setIndexFun={setIndex} num_of_cards = {total_num_of_cards}></NextCardButton> }
-                </div>
-            </div>
-            
-        </>
-    )
+export default async function PlayPage({ params }: PlayProps) {
+  const cards = await fetchCards(params.collection);
+    
+  return (
+    <>
+      <PlayPageClient cards= {cards}/>
+    </>
+  );
 }
