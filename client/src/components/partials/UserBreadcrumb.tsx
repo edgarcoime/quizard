e87593b8ca00c7fc7f77979e4d4dd67e852bfd55
Edgar Coime <e.coimeii@hotmail.com>
2024-11-10 10:08:58 -0800

    feat(ui): dynamically determine title truncation per crumb

--- client/src/components/partials/UserBreadcrumb.tsx
+++ client/src/components/partials/UserBreadcrumb.tsx
@@ -10,52 +10,79 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 
+interface BreadcrumbLink {
+  title: string;
+  url: string;
+}
+
 const SETTINGS = {
-  titleLen: 15,
+  maxWidthChar: 45,
 };
 
 export default function UserBreadcrumb() {
   const currentPath = usePathname();
 
-  const { titleLen } = SETTINGS;
+  const { maxWidthChar } = SETTINGS;
+
+  function createBreadcrumbLinks(
+    titleLinks: string[],
+    links: string[],
+  ): BreadcrumbLink[] {
+    // calculate approx combined element length
+    const combinedLength = titleLinks.reduce(
+      (acc, curr) => acc + curr.length,
+      0,
+    );
+
+    const titleLen = Math.floor(maxWidthChar / titleLinks.length);
+    console.log("titleLen", titleLen);
+    console.log("combinedLength", combinedLength);
+
+    return combinedLength > maxWidthChar
+      ? titleLinks.map((path, index) => ({
+          title:
+            path.length > titleLen ? path.slice(0, titleLen) + "..." : path,
+          url: "/" + paths.slice(0, index + 2).join("/"),
+        }))
+      : titleLinks.map((path, index) => ({
+          title: path,
+          url: "/" + paths.slice(0, index + 2).join("/"),
+        }));
+  }
 
   const paths = currentPath.split("/").slice(1);
   // slice(2) to remove domain/id
   const breadcrumbPaths = currentPath.split("/").slice(2);
-  const links: { title: string; url: string }[] = useMemo(
-    () =>
-      breadcrumbPaths.map((path, index) => ({
-        title: path.length > titleLen ? path.slice(0, titleLen) + "..." : path,
-        url: "/" + paths.slice(0, index + 1).join("/"),
-      })),
+  const links: BreadcrumbLink[] = useMemo(
+    () => createBreadcrumbLinks(breadcrumbPaths, paths),
     [paths],
   );
 
-  console.log(links);
-
   return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        {links.map(({ title, url }, index) => {
-          const targetLength = links.length - 1;
-          const lastItem = index >= targetLength;
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          {links.map(({ title, url }, index) => {
+            const targetLength = links.length - 1;
+            const lastItem = index >= targetLength;
 
-          return lastItem ? (
-            <>
-              <BreadcrumbItem key={url}>
-                <BreadcrumbPage>{title}</BreadcrumbPage>
-              </BreadcrumbItem>
-            </>
-          ) : (
-            <>
-              <BreadcrumbItem key={url}>
-                <BreadcrumbLink href={url}>{title}</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-            </>
-          );
-        })}
-      </BreadcrumbList>
-    </Breadcrumb>
+            return lastItem ? (
+              <>
+                <BreadcrumbItem key={url}>
+                  <BreadcrumbPage>{title}</BreadcrumbPage>
+                </BreadcrumbItem>
+              </>
+            ) : (
+              <>
+                <BreadcrumbItem key={url}>
+                  <BreadcrumbLink href={url}>{title}</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+              </>
+            );
+          })}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </>
   );
 }
