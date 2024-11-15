de2a02865cbd335c145421f8ee1363d68e28020b
Edgar Coime <e.coimeii@hotmail.com>
2024-11-14 16:38:46 -0800

    fix: change settings for easier deployment

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,5 +1,4 @@
 // TODO: determine if this needs to be visible to the client. Prob not?
 export const ROOT_DOMAIN = process.env.NEXT_PUBLIC_HOST;
-export const NEXT_API_BASE_URL = `${ROOT_DOMAIN}/api`;
 export const API_BASE_URL = `${ROOT_DOMAIN}/api/py`;
 export const SERVER_VISIBLE_ONLY = process.env.SERVER_VISIBLE_ONLY;
