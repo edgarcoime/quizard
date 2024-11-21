35a3111ae042f3a8ae01560eb204a43e19afdea4
Edgar Coime <e.coimeii@hotmail.com>
2024-11-20 19:08:23 -0800

    feat: update create collection form

--- client/src/app/(with-layout)/id/[username]/(user)/new/page.tsx
+++ client/src/app/(with-layout)/id/[username]/(user)/new/page.tsx
@@ -12,17 +12,10 @@ export default function Page({
       {/* Page Header */}
       <div className="text-center">
         <h1 className="text-3xl font-bold text-gray-800">
-          <strong >Create</strong> a Collection
+          <strong>Create</strong> a Collection
         </h1>
         <p className="mt-2 text-gray-600">
           Organize and store all your cards in one place.
-        </p>
-      </div>
-
-      {/* Username Display */}
-      <div className="text-center">
-        <p className="text-gray-700 font-medium">
-          <span className="text-gray-500">Username:</span> {username}
         </p>
       </div>
 
