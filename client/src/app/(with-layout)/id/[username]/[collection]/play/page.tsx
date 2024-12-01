3bcdb2d47d052ffe0e2ff759dd33efbb19a76da7
AmarjotS82 <amarjot.sangha28@gmail.com>
2024-11-30 16:27:54 -0800

    Adding error message if no cards in collection when trying to use play feature

--- client/src/app/(with-layout)/id/[username]/[collection]/play/page.tsx
+++ client/src/app/(with-layout)/id/[username]/[collection]/play/page.tsx
@@ -8,10 +8,17 @@ interface PlayProps {
 
 export default async function PlayPage({ params }: PlayProps) {
   const cards = await fetchCards(params.collection);
-  
+  let cardsinCollection = true
+  if (cards.length == 0)
+    cardsinCollection = !cardsinCollection
+  console.log("cards in collection: " + cards.length)
+  console.log(cardsinCollection)
   return (
     <>
-      <PlayPageClient cards= {cards} username={params.username} collectionName={params.collection}/>
+      { cardsinCollection ?
+        <PlayPageClient cards= {cards} username={params.username} collectionName={params.collection}/> 
+        : <h1 className=" flex-row justify-self-center p-4 text-xl text-red-500">No question cards in collection! Add at least one card to quiz yourself!</h1>
+      }
     </>
   );
 }
