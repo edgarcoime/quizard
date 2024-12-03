43dfe497e13eeabc7ddeb772f9eb2b5ca3354e59
AmarjotS82 <113328370+AmarjotS82@users.noreply.github.com>
2024-12-03 11:24:52 -0800

    Amarjot s82/testing (#58)
    * Adding error message if no cards in collection when trying to use play feature
    
    * Added testing and paly feature cheking

--- client/src/app/(with-layout)/id/[username]/[collection]/play/AnswerForm.tsx
+++ client/src/app/(with-layout)/id/[username]/[collection]/play/AnswerForm.tsx
@@ -1,10 +1,11 @@
 
-
+// Import necessary dependencies and components
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import SubmitAnswerButton from "./submitAnswerButton";
 import { Submission } from "@/types/Submission";
 
+// Interface defining the props for the AnswerForm component
 interface AnserFormProps{
     answerArr: string[],
     setAnswerArr: Function,
@@ -13,14 +14,14 @@ interface AnserFormProps{
     feedbackArr: Submission[],
     setFeedbackArr: Function
 }
-
+// Function to handle updating the answer array
 function setAnswer(value: string, answerArr: string[], setAnswerArr:Function, index:number){
     const newAnswerArr = [...answerArr];
     newAnswerArr[index] = value; 
     setAnswerArr(newAnswerArr);
 
 }
-
+// Anser form componenent that returns the JSX element for the form
 export default function AnswerForm({answerArr, setAnswerArr, index, cardID, feedbackArr, setFeedbackArr}: AnserFormProps){
     console.log("typed answer: " + answerArr[index])
     
--- client/src/app/(with-layout)/id/[username]/[collection]/play/prevCardButton.tsx
+++ client/src/app/(with-layout)/id/[username]/[collection]/play/prevCardButton.tsx
@@ -1,11 +1,11 @@
 import { IoChevronBackSharp } from "react-icons/io5";
-
+//defines props
 interface prevBtnProps{
     index : number,
     setIndexFun: Function
 
 }
-
+// previous button to go to previous card returns the JsX if not on first question
 export default function PrevCardButton({index, setIndexFun} : prevBtnProps){
     return(
         <>
